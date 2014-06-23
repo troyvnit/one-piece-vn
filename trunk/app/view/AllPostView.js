a2fee7fdbacc5b0afc49ad3a3f809e02169e8206
troyuit@gmail.com <troyuit@gmail.com@fc6d5b95-8758-4d46-d0d8-7bc1e108873b>
2014-06-23 03:05:45 +0000

    Startup

---
+++ trunk/app/view/AllPostView.js
@@ -0,0 +1,42 @@
+﻿Ext.define('OnePiece.view.AllPostView', {
+    extend: 'Ext.Container',
+    xtype: 'allPostView',
+    requires: [
+        'Ext.TitleBar',
+        'Ext.carousel.Carousel'
+    ],
+    config: {
+        scrollable: true,
+        items: [
+            {
+                xtype: 'toolbar',
+                title: 'All Post',
+                items: [
+                    {
+                        text: 'Back',
+                        ui: 'back'
+                    },
+                    {
+                        xtype: 'spacer'
+                    },
+                    {
+                        text: 'Save',
+                        ui: 'confirm'
+                    }
+                ]
+            },
+            {
+                xtype: 'panel',
+                height: '1000px',
+                itemId: 'contentPanel',
+                scrollable: true,
+                items: [
+                    {
+                        xtype: 'gallery',
+                        height: '1000px'
+                    }
+                ]
+            }
+        ]
+    }
+});
